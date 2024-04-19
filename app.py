b34605ba01c2398acdea78655ce58eca53c88d3c
Mona <31501832@student.publicboard.ca>
2024-04-19 02:36:30 -0400

    be able to get information from form

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 makes and runs flask app
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 # display main page html at url "/"
 @app.route("/", methods=['GET','POST'])
 def home():
+    if request.method == 'POST':
+        incl_seasons = request.form["seasons"]
+        incl_crack = request.form["crack"]
+        print(incl_seasons, incl_crack)
     return render_template("app.html")
